ba04c53ce11f0ebe0ee234e92df8097c795b1d08
Juan Young Huh <juan.huh@evry.com>
2020-02-24 23:39:20 +0100

    x

--- lib/screens/sport_screen.dart
+++ lib/screens/sport_screen.dart
@@ -45,32 +45,33 @@ class SportList extends StatelessWidget {
           imageUrl: 'http://www.orebro.moodowon.net/gallery/tkd1.jpg',
           title: 'Taekwondo',
           subtitle: 'Vuxen & Barn',
-          description: 'Koreansk vapenlös kampsport som ställer höga krav på både fysisk och mental disciplin, vilket präglas av ödmjukhet och hårtarbete.',
+          description:
+              'Koreansk vapenlös kampsport som ställer höga krav på både fysisk och mental disciplin, vilket präglas av ödmjukhet och hårtarbete.',
           height: 250.0,
         ),
-//        SportCard(
-//          title: 'Taekwondo',
-//          description:
-//              'Koreansk vapenlös kampsport som ställer höga krav på både fysisk och mental disciplin, vilket präglas av ödmjukhet och hårtarbete.',
-//          imageUrl: 'http://www.orebro.moodowon.net/gallery/tkd1.jpg',
-//        ),
-        SportCard(
+        FullImageCard(
+          imageUrl: 'http://www.orebro.moodowon.net/gallery/gumdo/gd23.jpg',
           title: 'Chosun Haedong Gumdo',
+          subtitle: 'Vuxen & Barn',
           description:
               'Koreansk svärds konst som baseras sig på riktig strids teknik och utgör ett kamp system för mental, fysisk och andlig utveckling.',
-          imageUrl: 'http://www.orebro.moodowon.net/gallery/gumdo/gd23.jpg',
+          height: 250.0,
         ),
-        SportCard(
+        FullImageCard(
+          imageUrl: 'http://www.orebro.moodowon.net/gallery/hapkido/hk7.jpg',
           title: 'Hapkido',
+          subtitle: 'Vuxen & Barn',
           description:
               'Koreansk kampkonst som involverar cirkelrörelser, stora som små, som används både i defensivt och offensivt syfte.',
-          imageUrl: 'http://www.orebro.moodowon.net/gallery/hapkido/hk7.jpg',
+          height: 250.0,
         ),
-        SportCard(
+        FullImageCard(
+          imageUrl: 'http://www.orebro.moodowon.net/gallery/zen/zen1.jpg',
           title: 'Zen Meditation & Yoga',
+          subtitle: 'Vuxen & Barn',
           description:
               'Utöva dynamiska yogaformer med stor tyngdpunkt i biomekanik och kroppsförståelse. Att utöva yoga är bra för både kropp och själ.',
-          imageUrl: 'http://www.orebro.moodowon.net/gallery/zen/zen1.jpg',
+          height: 250.0,
         ),
       ],
     );
@@ -90,5 +91,4 @@ class InstructorList extends StatelessWidget {
       ],
     );
   }
-
 }
