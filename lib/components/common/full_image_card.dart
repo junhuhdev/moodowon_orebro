077dfeff82bb9bb5c1b21fed791652d8cfe2e23e
Juan Young Huh <juan.huh@evry.com>
2020-02-24 23:21:50 +0100

    x

--- lib/components/common/full_image_card.dart
+++ lib/components/common/full_image_card.dart
@@ -11,51 +11,56 @@ class FullImageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        new SizedBox.expand(
-          child: new Material(
-            borderRadius: new BorderRadius.circular(12.0),
-            child: new CachedNetworkImage(
-              placeholder: (context, url) => CircularProgressIndicator(),
-              imageUrl: imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        new SizedBox.expand(
-          child: new Container(
-            decoration: new BoxDecoration(
-              gradient: new LinearGradient(
-                colors: [Colors.transparent, Colors.black87],
-                begin: Alignment.center,
-                end: Alignment.bottomCenter,
+    return SizedBox(
+      height: 250.0,
+      child: Card(
+        child: Stack(
+          children: <Widget>[
+            new SizedBox.expand(
+              child: new Material(
+                borderRadius: new BorderRadius.circular(12.0),
+                child: new CachedNetworkImage(
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  imageUrl: imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-        ),
-        new Align(
-          alignment: Alignment.bottomLeft,
-          child: new Container(
-            padding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(title, style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)),
-                new Padding(padding: new EdgeInsets.only(bottom: 1.0)),
-                new Text(subtitle, style: new TextStyle(fontSize: 13.0, color: Colors.white)),
-                new Padding(padding: new EdgeInsets.only(bottom: 4.0)),
-                new Text(
-                  description,
-                  textAlign: TextAlign.start,
-                  style: TextStyle(fontSize: 11.0, color: Colors.white),
+            new SizedBox.expand(
+              child: new Container(
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                    colors: [Colors.transparent, Colors.black87],
+                    begin: Alignment.center,
+                    end: Alignment.bottomCenter,
+                  ),
                 ),
-              ],
+              ),
             ),
-          ),
+            new Align(
+              alignment: Alignment.bottomLeft,
+              child: new Container(
+                padding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(title, style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)),
+                    new Padding(padding: new EdgeInsets.only(bottom: 1.0)),
+                    new Text(subtitle, style: new TextStyle(fontSize: 13.0, color: Colors.white)),
+                    new Padding(padding: new EdgeInsets.only(bottom: 4.0)),
+                    new Text(
+                      description,
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 11.0, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
--- lib/screens/membership_screen.dart
+++ lib/screens/membership_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:moodowon_orebro/components/common/full_image_card.dart';
+import 'package:moodowon_orebro/components/sport/sport_header.dart';
 
 class MembershipScreen extends StatelessWidget {
   const MembershipScreen();
@@ -19,11 +21,32 @@ class MembershipScreen extends StatelessWidget {
         ),
         body: TabBarView(
           children: <Widget>[
-            Container(),
+            MembershipList(),
             Container(),
           ],
         ),
       ),
     );
   }
+}
+
+class MembershipList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        SportHeader(
+          title: 'Medlemskap',
+          description: 'Välj mellan tre av våra koreanska kampsporter och börja träna redan idag!',
+        ),
+        FullImageCard(
+          imageUrl: 'http://www.orebro.moodowon.net/img/pricing_tkd2.jpg',
+          title: 'Taekwondo',
+          subtitle: '1000 SEK per termin',
+          description: 'Barn & Ungdomgrupp 7-17 år 800 SEK',
+        ),
+      ],
+    );
+  }
+
 }
