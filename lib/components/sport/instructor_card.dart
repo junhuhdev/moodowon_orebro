23f6ca49eab145fb177477c7bd66355adea615d5
Juan Young Huh <juan.huh@evry.com>
2020-02-24 22:58:00 +0100

    x

--- lib/components/sport/instructor_card.dart
+++ lib/components/sport/instructor_card.dart
@@ -15,7 +15,7 @@ class InstructorCard extends StatelessWidget {
               height: 338.0,
               child: Card(
                 clipBehavior: Clip.antiAlias,
-                child: InstructorContent(),
+                child: Content(),
               ),
             )
           ],
@@ -25,22 +25,55 @@ class InstructorCard extends StatelessWidget {
   }
 }
 
-class SectionTitle extends StatelessWidget {
-  const SectionTitle({
-    Key key,
-    this.title,
-  }) : super(key: key);
-
-  final String title;
-
+class Content extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(4, 4, 4, 12),
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Text(title, style: Theme.of(context).textTheme.subhead),
-      ),
+    return Stack(
+      children: <Widget>[
+        new SizedBox.expand(
+          child: new Material(
+            borderRadius: new BorderRadius.circular(12.0),
+            child: new CachedNetworkImage(
+              placeholder: (context, url) => CircularProgressIndicator(),
+              imageUrl: 'http://www.orebro.moodowon.net/img/team/simonlim_bakgrund.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        new SizedBox.expand(
+          child: new Container(
+            decoration: new BoxDecoration(
+              gradient: new LinearGradient(
+                colors: [Colors.transparent, Colors.black87],
+                begin: Alignment.center,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+        ),
+        new Align(
+          alignment: Alignment.bottomLeft,
+          child: new Container(
+            padding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text('GM Mästare Simon Lim',
+                    style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white)),
+                new Padding(padding: new EdgeInsets.only(bottom: 1.0)),
+                new Text('Ledare & Instruktör', style: new TextStyle(fontSize: 13.0, color: Colors.white)),
+                new Padding(padding: new EdgeInsets.only(bottom: 4.0)),
+                new Text(
+                  'Mästare Simon är en flerfaldig koreansk mästare med internationella meriter. År 1988 grundade han sin egen förening, Örebro taekwondo föreningen.',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 11.0, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
