0632f8d043d16222ebbe99eabb9ed0d59e4acb7d
Juan Young Huh <juan.huh@evry.com>
2020-02-29 23:49:29 +0100

    x

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moodowon_orebro/components/common/full_background_card.dart';
+import 'package:moodowon_orebro/components/sport/sport_header.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen();
@@ -7,64 +8,57 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        constraints: BoxConstraints.expand(),
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
+      body: ListView(
+        children: <Widget>[
+          SportHeader(
+            title: 'Moodowon Örebro',
+            description:
+                'Ta hand om din kropp och sinne. Kom och träna med oss i Örebro vid Almbyskolans gymnastiksal.',
+          ),
+          SizedBox(height: 10.0),
+          Center(
+            child: Text(
+              'Vi erbjuder följande',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            physics: ScrollPhysics(),
             children: <Widget>[
-              SizedBox(height: 20.0),
-              Text(
-                'Ta hand om din kropp och sinne.',
+              FullBackgroundImage(
+                title: 'Taekwondo',
+                subtitle: '',
+                description: '',
+                height: 250,
+                imageUrl: 'http://www.orebro.moodowon.net/img/pricing_tkd2.jpg',
               ),
-              SizedBox(height: 10.0),
-              Text(
-                'Kom och träna Taekwondo med oss i Örebro.',
+              FullBackgroundImage(
+                title: 'Chosun Haedong Gumdo',
+                subtitle: '',
+                description: '',
+                height: 250,
+                imageUrl: 'https://i.pinimg.com/originals/55/7e/9c/557e9c9f565a61b58008a61a0842f497.jpg',
               ),
-              SizedBox(height: 10.0),
-              Text(
-                'Vi erbjuder följande.',
+              FullBackgroundImage(
+                title: 'Hapkido',
+                subtitle: '',
+                description: '',
+                height: 250,
+                imageUrl: 'https://www.tryimpact.com.au/wp-content/uploads/2018/01/Hapkido-1-black-background.jpg',
               ),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  children: <Widget>[
-                    FullBackgroundImage(
-                      title: 'Taekwondo',
-                      subtitle: '',
-                      description: '',
-                      height: 250,
-                      imageUrl: 'http://www.orebro.moodowon.net/img/pricing_tkd2.jpg',
-                    ),
-                    FullBackgroundImage(
-                      title: 'Chosun Haedong Gumdo',
-                      subtitle: '',
-                      description: '',
-                      height: 250,
-                      imageUrl: 'https://i.pinimg.com/originals/55/7e/9c/557e9c9f565a61b58008a61a0842f497.jpg',
-                    ),
-                    FullBackgroundImage(
-                      title: 'Hapkido',
-                      subtitle: '',
-                      description: '',
-                      height: 250,
-                      imageUrl:
-                          'https://www.tryimpact.com.au/wp-content/uploads/2018/01/Hapkido-1-black-background.jpg',
-                    ),
-                    FullBackgroundImage(
-                      title: 'Zen Meditation & Yoga',
-                      subtitle: '',
-                      description: '',
-                      height: 250,
-                      imageUrl: 'http://www.orebro.moodowon.net/gallery/zen/zen1.jpg',
-                    )
-                  ],
-                ),
-              ),
+              FullBackgroundImage(
+                title: 'Zen Meditation & Yoga',
+                subtitle: '',
+                description: '',
+                height: 250,
+                imageUrl: 'http://www.orebro.moodowon.net/gallery/zen/zen1.jpg',
+              )
             ],
           ),
-        ),
+        ],
       ),
     );
   }
