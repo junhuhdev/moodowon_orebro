ee8881b63a62ef78043c851bf21c477f44123b27
Juan Young Huh <juan.huh@evry.com>
2020-02-29 22:21:02 +0100

    x

---
+++ lib/components/registration/email_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmailForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _EmailFormState();
+  }
+}
+
+class _EmailFormState extends State<EmailForm> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ContactScreen extends StatelessWidget {
   const ContactScreen();
@@ -8,7 +12,7 @@ class ContactScreen extends StatelessWidget {
     List<String> tabs = ['Kontakt', 'Hitta oss'];
 
     return DefaultTabController(
-      length: 2,
+      length: tabs.length,
       child: Scaffold(
         appBar: TabBar(
           labelColor: Colors.indigo,
@@ -19,19 +23,42 @@ class ContactScreen extends StatelessWidget {
         ),
         body: TabBarView(
           children: <Widget>[
-            Container(),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Text('Hitta oss'),
-                  Text('Vi finns i Almbyskolan Örebro, nära universitetet.'),
-
-                ],
-              ),
-            ),
+            ContactInfo(),
+            LocationInfo(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ContactInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class LocationInfo extends StatelessWidget {
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+//    return WebviewScaffold(
+//      url: 'https://goo.gl/maps/f7Z5mx8wsSANdar99',
+//      appCacheEnabled: true,
+//
+//    );
+    return WebView(
+//      initialUrl: 'https://goo.gl/maps/f7Z5mx8wsSANdar99',
+      initialUrl: Uri.dataFromString(
+          '<html><body><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2039.3845592276034!2d15.24308125222361!3d59.25972398154447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465c14509b3d6e97%3A0x56b8f9d543c87356!2sProstv%C3%A4gen%2010B%2C%20702%2084%20%C3%96rebro!5e0!3m2!1ssv!2sse!4v1583007287711!5m2!1ssv!2sse" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe></body></html>',
+          mimeType: 'text/html')
+          .toString(),
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller.complete(webViewController);
+      },
     );
   }
 }
