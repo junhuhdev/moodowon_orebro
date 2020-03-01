e0ea50e51b4aedbe4dd9e28d81a20b0117ade82f
Juan Young Huh <juan.huh@evry.com>
2020-03-01 01:09:17 +0100

    x

--- lib/components/contact/map_card.dart
+++ lib/components/contact/map_card.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:moodowon_orebro/components/contact/marker.dart';
 import 'package:moodowon_orebro/components/contact/static_map_provider.dart';
+import 'package:moodowon_orebro/components/sport/sport_header.dart';
 
 class MapCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String apiKey = "AIzaSyBM9V5LdnczVfAC9v6tbG0QanV8tE5lq48";
     ThemeData theme = Theme.of(context);
-    const _lat = 35.6957954;
-    const _lang = 139.69038920000003;
+    const _lat = 59.259883;
+    const _lang = 15.245307;
     final Marker _marker = new Marker("place", "", _lat, _lang, color: theme.accentColor);
     var staticMapProvider = new StaticMapProvider(apiKey);
     Uri staticMapUri = staticMapProvider.getStaticUriWithMarkers([_marker], 14, width: 900, height: 450);
@@ -18,13 +19,33 @@ class MapCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
+          SportHeader(
+            title: 'Hitta oss',
+            description: 'Vi finns i Örebro vid Almbyskolans gymnastiksal',
+          ),
           new Stack(
             children: <Widget>[
               new Container(
                 child: new Image.network(staticMapUri.toString()),
               )
             ],
-          )
+          ),
+          new Container(
+            color: theme.primaryColor,
+            padding: const EdgeInsets.all(16.0),
+            child: new Row(
+              children: [
+                Icon(Icons.map, color: Colors.white),
+                SizedBox(width: 10.0),
+                new Expanded(
+                  child: new Text(
+                    'Prostvägen 10B, 702 84 Örebro',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
